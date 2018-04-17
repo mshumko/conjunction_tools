7dca457acd53210eb5c9fc79418ba1f4580237b7
Mykhaylo Mike Shumko <msshumko@gmail.com>
2018-04-17 14:43:48 -0600

    Added a new folder to process campaign 15 conjunctions.

---
+++ 2018_04_predicted_ephem/proc_conjunctions.py
@@ -0,0 +1,47 @@
+# Proc conjunctions
+import glob
+import os
+import sys
+import time
+import numpy as np
+from datetime import datetime
+
+sys.path.insert(0, os.path.join(os.path.dirname( __file__ ), '..'))
+import conjunctiontools
+
+dL = 1
+dMLT = 1
+lowerL = 3
+FDIR = '/home/mike/research/conjunction-tools/2018_04_predicted_ephem'
+
+for fb_id in [3, 4]:
+    for rb_id in ['A', 'B']:
+        FNAME_A = ('FU{}_2018-04-18_2018-05-31_magephem_pre.txt'.format(fb_id))
+        FNAME_B = ('rbsp{}_2018-04-18_2018-05-31_magephem_pre.txt'.format(
+                    rb_id.lower()))
+        SAVE_NAME = ('FU{}_RBSP{}_camp15_conjunctions_pre.txt'.format(
+                    fb_id, rb_id))
+
+        cCalc = conjunctiontools.MagneticConjunctionCalc(
+            FDIR, FNAME_A, FDIR, FNAME_B,
+            mission_id_A='FU{}'.format(fb_id), 
+            mission_id_B='RBSP{}'.format(rb_id), verbose=True, 
+            Lthresh=dL,  MLTthresh=dMLT, lowerL=lowerL) 
+            
+        cCalc.magephemB[cCalc.timeKeyB] = np.array([i.replace(tzinfo=None)  
+            for i in cCalc.magephemB[cCalc.timeKeyB]])
+        
+        cCalc.periodic_data_indicies() 
+        #cCalc.non_periodic_data_indicies()
+        cCalc.calc_magnetic_seperation()
+        cCalc.calc_L_MLT_conjunction_delay(0)
+        cCalc.lower_L_bound()
+        #try: # If no conjunctions are found, log it, and move on.
+        cCalc.calc_conjunction_duration()
+        # except AssertionError as err:
+        #     print(err)
+        #     logging.info('{} \n No conjunctions found on day {} \n {} \n'.format(
+        #         datetime.now(), np.array(datesA)[i], str(err)))
+        #     continue
+        #cCalc.plotLMLTandDLDMLT(A_id='FU'+str(fb_id), B_id='RBSP'+str(rb_id))
+        cCalc.save_to_file(save_name=SAVE_NAME, save_dir=FDIR)
---
+++ 2018_04_predicted_ephem/proc_fb_ephem.py
@@ -0,0 +1,35 @@
+# This script processes FIREBIRD ephemeris into the future.
+import sys
+import os
+import dateutil.parser
+import numpy as np
+
+import spacepy.datamodel
+
+sys.path.insert(0, '/home/mike/research/firebird/data_processing/'         'magnetic_ephemeris')
+import make_magnetic_ephemeris
+
+for sc_id in [3, 4]:
+    ephemDir = '/home/mike/research/conjunction-tools/2018_04_predicted_ephem'
+    ephemName = ('FU{}_2018-04-18_2018-05-31'
+                '_LLA_ephemeris_pre.csv'.format(sc_id))
+    saveName = ephemName.split('_')
+    saveName[-2] = 'TLE_magephem.txt'
+    saveName = '_'.join(saveName)
+    print(saveName)
+
+    # Read in the ephemeris
+    #ephem = spacepy.datamodel.readJSONheadedASCII(
+    #    os.path.join(ephemDir, ephemName))
+    #ephem['dateTime'] = np.array([dateutil.parser.parse(i) for i in
+    #    ephem['dateTime']])
+
+    # Now run the magnetic ephemeris code
+    magEphem = make_magnetic_ephemeris.Magnetic_Ephemeris(
+        'FB', ephemDir=ephemDir, ephemName=ephemName, singleKp=20, sc_id=str(sc_id),
+        outputName=saveName, outputDir=ephemDir)
+    magEphem.run_mag_model(multiprocess=True, nonPeriodicFlag=False,
+        mirrorPointAlt=False)
+    # magEphem.outputName = saveName
+    # magEphem.outputDir = ephemDir
+    magEphem.saveToFile(daily=False)
