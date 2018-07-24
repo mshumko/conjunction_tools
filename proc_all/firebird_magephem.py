ed19d382cec9a230b71bf1f6379512ef621701af
mshumko <msshumko@gmail.com>
2018-07-23 21:43:34 -0600

    Wrote the script to make the FIREBIRD magephem in the same way as the RBSP magephem. Files are saved in JSON headed ASCII.

--- proc_all/firebird_magephem.py
+++ proc_all/firebird_magephem.py
@@ -9,10 +9,32 @@ import IRBEM
 sys.path.insert(0, '/home/mike/research/firebird/data_processing/magnetic_ephemeris')
 import make_magnetic_ephemeris
 
-inDir = '/home/mike/research/conjunction-tools/proc_all/rbsp_camp_ephem'
-outDir = '/home/mike/research/conjunction-tools/proc_all/rbsp_camp_magephem'
+inDir = '/home/mike/research/firebird/Datafiles/'
+outDir = '/home/mike/research/conjunction-tools/proc_all/firebird_camp_magephem'
 # If none, will look for existing kp in data. Change to a number when forward propagating!
 singleKp = None 
 
 overwrite = False # Flag to overwrite any magephem files already generated
-inPaths = sorted(glob.glob(os.path.join(inDir, '*')))
+inPaths = sorted(glob.glob(os.path.join(inDir, '**/*LLA_camp*'), recursive=True))
+# Now get a list of outFiles (magephem files to save to).
+inBasename = [os.path.basename(f) for f in inPaths]
+inBasenameSplit = [x.split('_') for x in inBasename]
+outBasename = ['_'.join([x[0], x[2], 'magephem.txt']) for x in inBasenameSplit]
+outPaths = [os.path.join(outDir, x) for x in outBasename]
+
+# Now loop over every file, and generate the corresponding
+# magephem file. If the file already exists, check is overwrite
+# flag is set.
+for inF, outF in zip(inPaths, outPaths):
+    if not overwrite and not os.path.isfile(outF):
+        # Process the magnetic ephemeris.
+        print('Processing {} -> {}'.format(os.path.basename(inF), os.path.basename(outF)))
+        magEphem = make_magnetic_ephemeris.Magnetic_Ephemeris(
+            'fb', ephemDir=os.path.dirname(inF), ephemName=os.path.basename(inF), 
+             outputDir=os.path.dirname(outF), outputName=os.path.basename(outF),
+             singleKp=singleKp)
+        magEphem.run_mag_model(multiprocess=True, nonPeriodicFlag=False,
+            mirrorPointAlt=False)
+        magEphem.outputName = os.path.basename(outF)
+        magEphem.outputDir = os.path.dirname(outF)
+        magEphem.saveToFile(daily=False)
